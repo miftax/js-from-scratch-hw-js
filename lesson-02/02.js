e5c6c9d6870d29997852dca8feff296ed6b0bc22
miftax <miftax802@gmail.com>
2026-04-19 14:53:55 +0300

    changes

--- lesson-02/02.js
+++ lesson-02/02.js
@@ -11,10 +11,10 @@ let dice1; // your code
 let dice2; // your code
 let isWinningDouble; // your code
 
-dice1 = Math.floor(Math.random() * 6)+1;
-dice2 = Math.floor(Math.random() * 6)+1;
-if (dice1===dice2 && (dice1&&dice2)>3) {
-  isWinningDouble=true
+dice1 = Math.floor(Math.random() * 6) + 1;
+dice2 = Math.floor(Math.random() * 6) + 1;
+if (dice1 === dice2 > 3) {
+  isWinningDouble = true;
 }
 
 console.log("Первый бросок: " + dice1);
