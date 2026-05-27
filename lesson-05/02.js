632c41ceaadb8db007e3fd2b982afe85d0044f2a
miftax <miftax802@gmail.com>
2026-05-27 12:18:26 +0300

    les5.02

--- lesson-05/02.js
+++ lesson-05/02.js
@@ -35,4 +35,13 @@ const gallery = {
   'The Scream': 'Edvard Munch',
 }
 
-function updateGallery() {}
+function updateGallery(gallery, artwork, newValue) {
+    if (gallery[artwork] === undefined) {
+        gallery[artwork] = newValue;
+    } else {
+        gallery[artwork] = newValue;
+    }
+}
+
+updateGallery(gallery, 'Mona Lisa', 'Leonardo da Vinci, 1503-1506')
+updateGallery(gallery, 'The Persistence of Memory', 'Salvador Dali')
