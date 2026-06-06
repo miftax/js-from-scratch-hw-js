421ef19585e222f71f4c4a10721892a4118c9647
miftax <miftax802@gmail.com>
2026-06-06 08:12:19 +0300

    les06

--- lesson-06/02.js
+++ lesson-06/02.js
@@ -23,7 +23,7 @@ const doubledNumbers = map(numbers, (element, index) => {
 
 console.log(doubledNumbers) // Должен вывести: [2, 4, 6, 8, 10]
 */
-
+const numbers = [1, 2, 3, 4, 5]
 const map = (array, callback) => {
     const result = [];
     for (let i = 0; i < array.length; i++) {
@@ -36,3 +36,4 @@ const map = (array, callback) => {
 const doubledNumbers = map(numbers, (element, index) => {
   return element * 2
 })
+console.log(doubledNumbers);
